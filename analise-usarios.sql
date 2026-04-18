a5aef38da09afe2c1718374944a495a9315ec5b8
Roberto Ferreira <138168507+robertojff@users.noreply.github.com>
2026-04-18 18:34:34 -0300

    Add files via upload

---
+++ analise-usarios.sql
@@ -0,0 +1,11 @@
+/*INSERT INTO usuarios(nome,email,data_nascimento)
+VALUES
+('Weliton Rodrigues','weliton34@hotmail.com','25/07/2001'),
+('Leonardo Paes','leonardopaes12@gmail.com','21/02/1999'),
+('Ingrid Maria','ingridm11@gmail.com','31/01/2005'),
+('João Pessoa','joao-pessoa190@gmail.com','14/05/2002'),
+('Cleiton Paes','cleitonpie@gmail.com','10/02/2001'),
+('Adalberto De Jesus Ferreira','adalbertojff3333@gmail.com','24/02/1958');*/
+
+SELECT * FROM usuarios;
+
