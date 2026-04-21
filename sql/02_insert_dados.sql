9b4d39aa30abc41c33b2450bfa2666540fb45583
Roberto Ferreira <138168507+robertojff@users.noreply.github.com>
2026-04-21 19:22:22 -0300

    Update 02_insert_dados.sql

--- sql/02_insert_dados.sql
+++ sql/02_insert_dados.sql
@@ -1,1 +1,20 @@
+-- =========================
+-- INSERÇÃO DE DADOS
+-- =========================
 
+-- Inserindo usuários
+INSERT INTO usuarios(nome, email, data_nascimento)
+VALUES
+('Weliton Rodrigues','weliton34@hotmail.com','2001-07-25'),
+('Leonardo Paes','leonardopaes12@gmail.com','1999-02-21'),
+('Ingrid Maria','ingridm11@gmail.com','2005-01-31'),
+('João Pessoa','joao-pessoa190@gmail.com','2002-05-14'),
+('Cleiton Paes','cleitonpie@gmail.com','2001-02-10');
+
+-- Inserindo pedidos relacionados aos usuários
+INSERT INTO pedidos(usuario_id)
+VALUES
+(1),
+(2),
+(1),
+(3);
