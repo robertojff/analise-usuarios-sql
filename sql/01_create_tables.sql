598c91eaa419313c438a3bddd79dcbbb88bf3938
Roberto Ferreira <138168507+robertojff@users.noreply.github.com>
2026-04-21 19:18:41 -0300

    Update 01_create_tables.sql

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,3 +1,8 @@
-SELECT * FROM usuarios;
-
-SELECT COUNT(*) FROM usuarios;
+-- Criando uma tabela de usuários
+CREATE TABLE usuarios(
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  data_nascimento DATE NOT NULL,
+  data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+); 
